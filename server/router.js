8b46b676c5218fb6810e997abd6c53ccf804c9f5
Devon Brosch <devon@brosch.org>
2017-09-18 14:40:42 -0400

    mod router.js to post db

--- server/router.js
+++ server/router.js
@@ -1,11 +1,17 @@
-module.exports = function(app){
+var Auth = require('./controllers/auth'); //include the authorization controller
+var User = require('./models/user'); //include the User model
 
-	app.get('/', function(req, res, next){
-		res.send("HELLO HOMEPAGE");
-	});
 
-	app.get('/signup', function(req, res, next){
-		res.send("Hey folks, Thanks for signing up!");
-	});
+module.exports = function(app){ //sets routes
+
+	app.post('/signup', Auth.signup);
+
+	// app.get('/', function(req, res, next){
+	// 	res.send("HELLO HOMEPAGE");
+	// });
+
+	// app.get('/signup', function(req, res, next){
+	// 	res.send("Hey folks, Thanks for signing up!");
+	// });
 };
 
