a8757508d807bffd5eaaac8eaef4039cd90fe3e8
Devon Brosch <devon@brosch.org>
2017-09-18 11:04:09 -0400

    mod index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.json({type: '*/*'}));
 var port = process.env.PORT || 3000;
 var server = http.createServer(app);
+var router = require('./router');
+
+app.use(bodyParser.json({type: '*/*'}));
+router(app);
+
+
 
 server.listen(port);
 console.log('Server listening on ' + port);
