08ad63dbf950b88c22dc20f0ca59f7024d23ed41
Devon Brosch <devon@brosch.org>
2017-09-19 15:32:18 -0400

    added videos and search

---
+++ client/src/components/video/video.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search-bar';
+import VideoDetail from './video-detail';
+
+const API_KEY = 'AIzaSyBn3UdM8oBO2Txu4i3OdfSoZuJmFOXJNu8';
+
+class Video extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('bucketlist');
+	}
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+	render(){
+		const videoSearch = _.debounce((term)=>{ this.videoSearch(term) }, 300);
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+			</div>
+		);
+	}
+}
+export default Video;
