16c545762f1fbfcf6fb05026bc585991bd1f8e00
Devon Brosch <devon@brosch.org>
2017-09-19 14:27:31 -0400

    navbar

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+//letting us create some routes on the front end
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+var createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+		</Route>
+	</Router>
+</Provider>
+, document.querySelector('.container'));
