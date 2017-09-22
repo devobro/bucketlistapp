6cbd762b2ffeba566096b8138bf680666f192e8d
Devon Brosch <devon@brosch.org>
2017-09-22 16:24:08 -0400

    added post get and delete

--- server/router.js
+++ server/router.js
@@ -18,4 +18,6 @@ module.exports = function(app){ //sets routes
 	app.post('/signin', requireSignin, Auth.signin);
 	app.post('/newitem', requireAuth, BucketList.addBucketList);
 	app.get('/items', requireAuth, BucketList.fetchBucketLists);
+	app.get('/items/:id', requireAuth, BucketList.fetchBucketList);
+	app.delete('/items/:id', requireAuth, BucketList.deleteBucketList);
 };
