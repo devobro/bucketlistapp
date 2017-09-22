32bae107b3491c05fa5ba1ef825e03789a2bef93
Devon Brosch <devon@brosch.org>
2017-09-22 09:54:04 -0400

    added auth signup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,6 +24,21 @@ export function signinUser({ email, password }){
 	};
 };
 
+export function signupUser({ email, password }){
+	return function(dispatch){
+		//Submit email/password to the server
+		axios.post(`${ROOT_URL}/signup`, { email, password})
+		.then(response => {
+			dispatch({ type: AUTH_USER });
+
+			//update the token
+			localStorage.setItem('token', response.data.token);
+			browserHistory.push('/newitem');
+		})
+		.catch(response => dispatch(authError("Bad login info")));
+	}
+}
+
 export function signoutUser(){
 	localStorage.removeItem('token');
 
